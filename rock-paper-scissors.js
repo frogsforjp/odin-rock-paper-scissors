1c1243cacfe01cecf20d8af13994859d96ff8e9a
John <18509053+frogsforjp@users.noreply.github.com>
2025-07-18 13:51:09 -0400

    feat: Add initial rock-paper-scissors.js
    Adds a javascript file that only contains pseudocode for now.
    This is just a rough draft that I will likely revise as I start writing.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,39 @@
+/*
+
+get Human Choice
+prompt the user for a choice between rock, paper or scissors
+(for this assignment assume the user always puts in a valid input)
+(also make the parameters case-insensitive)
+
+playerchoice = rock
+playerchoice = paper
+playerchoice = scissors
+
+get Computer Choice
+select a random number 1-3 to choose rock, paper or scissors
+
+check who won
+
+    function for player choosing rock
+        if computer chose rock, tie
+        if computer chose paper, lose. +1 computer score
+        if computer chose scissors, win. +1 human score
+
+    function for player choosing paper
+        if computer chose rock, win. +1 human score
+        if computer chose paper, tie
+        if computer chose scissors, lose. +1 computer score
+
+    function for player choosing scissors
+        if computer chose rock, lose. +1 computer score
+        if computer chose paper, win. +1 human score
+        if computer chose scissors, tie
+
+keep track of human score and computer score and print it after every
+round. Assign the variables 0 to start.
+
+prompt to restart, play again, or quit
+
+5 rounds
+
+*/

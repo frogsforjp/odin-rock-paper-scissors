46978608135f01e80e94329d7f886875f0beb955
John <18509053+frogsforjp@users.noreply.github.com>
2025-08-11 16:20:14 -0400

    feat: Add UI with buttons and text results
    The game will now play in the browser rather than the console.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,37 +1,24 @@
-//I plan to be more mindful of global scope in the future when it comes to variables
-
 let humanScore = 0;
 let computerScore = 0;
 let gameCount = 0;
 
 function getComputerChoice() {
-    randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        return "rock";
-    } else if (randomNumber === 1) {
-        return "paper";
-    } else if (randomNumber === 2) {
-        return "scissors";
-    }
-}
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+};
 
-function getHumanChoice () {
-    let choice = prompt("Enter rock, paper, or scissors");
-    if (choice === "shoe") {
-        humanScore += 9000;
-        gameCount += 5;
-        console.log(`You unlocked the secret ending.`)
-        endGame();
-    } else {
-        return choice.toLowerCase();
-    }
-}
-
-//I chose to ignore ties affecting the game count and score
+function getHumanChoice(button) {
+    return button.target.textContent.toLowerCase();
+};
 
 function playRound(humanChoice, computerChoice) {
+    const newResult = document.createElement('p');
+
     if (humanChoice === computerChoice) {
-        console.log(`The computer chose ${computerChoice}.\nYou tied this round. Your score:${humanScore} Computer score:${computerScore}`);
+        newResult.textContent = `The computer chose ${computerChoice}.\nYou tied this round. Your score:${humanScore} Computer score:${computerScore}`;
+        // I chose to not increase the game count or score in the event of a 
+        // tie since that's how it usually works in rock, paper, scissors.
     } else if (
         (humanChoice ==="rock" && computerChoice === "scissors") || 
         (humanChoice === "paper" && computerChoice === "rock") || 
@@ -39,7 +26,7 @@ function playRound(humanChoice, computerChoice) {
     ) {
         humanScore++;
         gameCount++;
-        console.log(`The computer chose ${computerChoice}.\nYou won this round! Your score:${humanScore} Computer score:${computerScore}`);
+        newResult.textContent = `The computer chose ${computerChoice}.\nYou won this round! Your score:${humanScore} Computer score:${computerScore}`;
     } else if (
         (humanChoice === "rock" && computerChoice === "paper") || 
         (humanChoice === "paper" && computerChoice === "scissors") || 
@@ -47,31 +34,44 @@ function playRound(humanChoice, computerChoice) {
     ) {
         computerScore++;
         gameCount++;
-        console.log(`The computer chose ${computerChoice}.\nYou lost this round. Your score:${humanScore} Computer score:${computerScore}`);
+        newResult.textContent = `The computer chose ${computerChoice}.\nYou lost this round. Your score:${humanScore} Computer score:${computerScore}`;
     }
-}
+    results.appendChild(newResult);
+};
 
-function playGame() {
-    while (gameCount < 5) {
-        const humanSelection = getHumanChoice();
+function playGame(button) {
+    if (gameCount < 5) {
+        const humanSelection = getHumanChoice(button);
         const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
-    }
-    endGame();
-}
+        };
+    if (gameCount >= 5) {
+        endGame();
+    };
+};
 
-//I created a endGame() function so I could use it for when the player inputs shoe in getHumanChoice() in addition to it's use inside the playGame() function.
+// I created a separate function for endgame because I initially used it in two
+// places. This might need cleaning up.
 
 function endGame() {
+    const newResult = document.createElement('p');
+
     if (humanScore > computerScore) {
-        console.log(`\u{1F389}CONGRATS, YOU WON THE GAME!!!\u{1F389} \nYour final score: ${humanScore}. Computer final score: ${computerScore}`)
+        newResult.textContent = `\u{1F389}CONGRATS, YOU WON THE GAME!!!\u{1F389} \nYour final score: ${humanScore}. Computer final score: ${computerScore}`;
     } else if (computerScore > humanScore) {
-        console.log(`You lost the game. :( \nYour final score: ${humanScore}. Computer final score: ${computerScore}`)
+        newResult.textContent = `You lost the game. :( \nYour final score: ${humanScore}. Computer final score: ${computerScore}`;
     } else if (computerScore === humanScore) {
-        console.log(`Somehow it's a tie. \nYour final score: ${humanScore}. Computer final score: ${computerScore}`)
+        newResult.textContent = `Somehow it's a tie. \nYour final score: ${humanScore}. Computer final score: ${computerScore}`;
     } else {
-        console.log("Something went wrong. I'm new to coding so don't blame me!")
+        newResult.textContent = "Something went wrong. I'm new to coding so don't blame me!";
     }
+
+    results.appendChild(newResult);
 }
 
-playGame();
+const buttons = document.querySelectorAll('.btn');
+const results = document.querySelector('.results');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (playGame));
+});
